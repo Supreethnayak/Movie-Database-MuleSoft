799adf1f7571ebd6791f5719aa28dd62a0ace112
Supreeth Nayak <59947774+Supreethnayak@users.noreply.github.com>
2021-10-09 17:18:41 +0530

    Update connect.php

--- connect.php
+++ connect.php
@@ -2,7 +2,7 @@
     $servername = "localhost";
     $username = "root";
     $password = "";
-    $database = "movie";
+    $database = "movies";
 
     $conn = mysqli_connect($servername,$username,$password,$database);
 ?>
